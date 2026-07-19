0dd58ba5cb3fae665939dbd788359653e7dfab01
SarahLBradburn <sarah.bradburn@hp.com>
2026-07-19 17:57:05 -0600

    Switched to animation player, added buttercup, changed but didn't totally fix walls

--- bunny-simulator/player/player.gd
+++ bunny-simulator/player/player.gd
@@ -1,44 +1,34 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var input_vector = Vector2.ZERO
 const SPEED = 200.0
-var latest_direction = "right"
 
-func _physics_process(delta):
-		# Get player input (-1 to 1) for all four directions
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	# Move the character using physics
-	velocity = input_vector * SPEED
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 
-	move_and_slide()
-	
-	# Change animations based on movement
-	if velocity.length() > 0:
-		if abs(velocity.x) > abs(velocity.y):
-			if velocity.x > 0:
-				animated_sprite_2d.play("walk_right")
-				latest_direction = "right"
-			else:
-				animated_sprite_2d.play("walk_left")
-				latest_direction = "left"
-		else:
-			if velocity.y > 0:
-				animated_sprite_2d.play("walk_down")
-				latest_direction = "down"
-			else:
-				animated_sprite_2d.play("walk_up")
-				latest_direction = "up"
+
+
+func _physics_process(delta: float) -> void:
+	var state = playback.get_current_node()	
+	match state:
+		"MoveState": move_state(delta)
+		
+func move_state(delta: float) -> void:
+	input_vector = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+			
+	if input_vector != Vector2.ZERO:
+		#y-axis is counterintuative in godot (and most game engines)
+		#this lets us set up the animations intuatively in the blend 
+		#state and then just inverse the direction calculation
+		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
 				
-	else:
-		if latest_direction == "right":
-			animated_sprite_2d.play("stand_right")
-		if latest_direction == "left":
-			animated_sprite_2d.play("stand_left")	
-		if latest_direction == "up":
-			animated_sprite_2d.play("stand_up")
-		if latest_direction == "down":
-			animated_sprite_2d.play("stand_down")
+		update_blend_positions(direction_vector)	
 		
+	velocity = input_vector * SPEED
+	move_and_slide()
 
-		
+func update_blend_positions(direction_vector: Vector2) -> void:
+	#Calling functions will go down the tree 
+	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/WalkState/blend_position", direction_vector)
