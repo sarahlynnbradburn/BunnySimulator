6b28b3d384fd687b00ea52762e0843e76fec7cc8
SarahLBradburn <sarah.bradburn@hp.com>
2026-08-18 09:09:03 -0600

    basic groom heart animation setup

--- bunny-simulator/buttercup/buttercup.gd
+++ bunny-simulator/buttercup/buttercup.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+var HEART_EFFECT = preload("res://buttercup/heart_effect.tscn")
+
+
+func _ready() -> void:
+	SignalBus.groom_buttercup.connect(play_heart_animation)
 
 
 func _on_area_2d_player_entered() -> void:
@@ -11,3 +16,8 @@ func _on_area_2d_player_entered() -> void:
 func _on_area_2d_player_exited() -> void:
 	print("player exited signal recieved by buttercup")
 	playback.travel("stand")
+
+func play_heart_animation() -> void:
+	var heart_instance = HEART_EFFECT.instantiate()
+	get_tree().current_scene.add_child(heart_instance)
+	heart_instance.global_position = global_position
